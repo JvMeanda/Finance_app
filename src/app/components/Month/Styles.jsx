5ee53686ae86ee18addd1bf5b2378036dde098a1
João Victor Barroso Meanda <meanda.barroso@gmail.com>
2024-09-05 15:25:18 -0300

    V-1.03 adaptive_icon, icone splash add

--- src/app/components/Month/Styles.jsx
+++ src/app/components/Month/Styles.jsx
@@ -8,13 +8,11 @@ export const styles = StyleSheet.create({
     borderRadius: theme.BorderRadius.md,
     padding: 6,
     textAlign: 'center',
-    // Estilos de sombra para iOS
-    shadowColor: '#000', // Cor da sombra
-    shadowOffset: { width: 0, height: 10 }, // Deslocamento da sombra
-    shadowOpacity: 0.16, // Opacidade da sombra
-    shadowRadius: 36, // Raio de difusão da sombra
-    // Estilos de elevação para Android
-    elevation: 10, // Elevação do botão, pode precisar ajustar
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.16,
+    shadowRadius: 36,
+    elevation: 10,
   },
 
   modalContainer: {
